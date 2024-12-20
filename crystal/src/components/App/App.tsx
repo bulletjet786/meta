dfbf2b1277b968a780c66e1810120a20fdee8f82
bulletjet <851234786@qq.com>
2024-12-20 20:52:33 +0800

    add somethging

--- crystal/src/components/App/App.tsx
+++ crystal/src/components/App/App.tsx
@@ -1,31 +1,9 @@
 import './App.css'
 
 import {useState} from 'react'
-import EChartsReact from "echarts-for-react";
 
 function App() {
     const [count, setCount] = useState(0)
-
-    const options = {
-        grid: {top: 8, right: 8, bottom: 24, left: 36},
-        xAxis: {
-            type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-        },
-        yAxis: {
-            type: 'value',
-        },
-        series: [
-            {
-                data: [820, 932, 901, 934, 1290, 1330, 1320],
-                type: 'line',
-                smooth: true,
-            },
-        ],
-        tooltip: {
-            trigger: 'axis',
-        },
-    };
 
     return (
         <div className="fixedPanel">
@@ -34,7 +12,6 @@ function App() {
                     count is {count}
                 </button>
             </div>
-            <EChartsReact option={options}/>;
         </div>
     )
 }
--- crystal/src/plugin/Crystal.tsx
+++ crystal/src/plugin/Crystal.tsx
@@ -34,25 +34,30 @@ export class AppWc extends r2wc(App, {
     }
 }
 
-// 注入Web Component styles
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-AppWc.initStyle(window.__crystal_styles)
-
-export function loadAppWcOn(element: HTMLElement) {
+function injectCrystalAppWc() {
+    // 注入Web Component styles
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    AppWc.initStyle(window.__crystal_styles)
     customElements.define("app-wc", AppWc)
 
-    element.innerHTML = `
-    <app-wc></app-wc>
-  `
+    const crystalHost = document.createElement('div');
+    crystalHost.id = 'crystal-host'
+    crystalHost.textContent = '<app-wc></app-wc>';
+    document.body.appendChild(crystalHost);
 }
 
-export function loadAppWcOnElementId(elementId: string): boolean {
-    const loadPoint = document.getElementById(elementId)
-    if (!loadPoint) {
-        return false
-    }
 
-    loadAppWcOn(loadPoint)
-    return true
+export function injectCrystal() {
+    if (document.readyState === 'loading') {
+        // 如果文档还在加载中，等待 DOMContentLoaded 事件
+        document.addEventListener('DOMContentLoaded', () => {
+            console.log('DOMContentLoaded event fired');
+            injectCrystalAppWc();
+        });
+    } else {
+        // 如果文档已经加载完成或处于 interactive 状态
+        console.log('Document is already loaded or interactive');
+        injectCrystalAppWc();
+    }
 }
