0aa4fc2a8e4c91c397a07fd0795ed3edbfbad083
bulletjet <851234786@qq.com>
2025-01-07 12:16:44 +0000

    remove mui, add antd

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,16 +1,16 @@
 import {useState} from 'react';
 import logo from './assets/images/logo-universal.png';
 import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import {Greet} from "../../../wailsjs/go/main/App";
 
 function App() {
 
-    // const steamControllerState, setSteamControllerStateState = useState("Disconnected")
+    const steamControllerState, setSteamControllerStateState = useState("Disconnected")
 
     return (
-        <div id="App">
+        <div>
             <h1>Steam伴侣</h1>
-            <p>状态：steamControllerState</p>
+            <p>状态：${steamControllerState}</p>
             <p>如何连接上Steam</p>
             <p>1. 在Steam快捷方式中添加启动选项：--，如图：</p>
             <p>2. 重新启动Steam。</p>
