30ca8e11611d3ac3ef074fd9f93575a339dee3a8
bulletjet <851234786@qq.com>
2025-02-17 10:11:05 +0800

    add disable resize

--- main.go
+++ main.go
@@ -34,6 +34,7 @@ func main() {
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
+		DisableResize:    true,
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		Bind: []interface{}{
 			machineService,
