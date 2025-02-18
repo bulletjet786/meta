68fc6be3948410295769c2bcf71961165e8243c1
bulletjet <851234786@qq.com>
2025-02-18 20:26:28 +0800

    add game panel code

---
+++ crystal/src/components/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import GamePanel from "@/pages/CrystalGamePanel/components/GamePanel";
+
+// 模仿 小黑盒的布局
+
+// [史低价格：xxx] [在线人数：xxx] [支持语言：xxx]
+// -价格历史- -多方比价- -促销节点-
+
+// 价格历史：
+// 区域 商店 货币（慢慢增加：仅国区、仅Steam、仅人民币）
+// 阶梯线图
+// 当前最低价格：区域 平台 货币 支付方式
+
+// 多方比价：
+// - 导航至 杉果
+// - 导航至 Game2K等
+// - 导航至 其他平台
+// 自己发行
+
+// 库中
+// -攻略论坛- -学习研究- -游戏搭子- -其他资源- -我的笔记- -分享笔记-
+// -发布游戏进度到我的描述/心情/状态
+// 私服列表，发布我的私服
+// 硬件推荐
+// 伴侣维护的QQ交流群
+
+
+// 攻略论坛：维护者
+// - xxxx
+// - xxxx
+// + 我有推荐
+
+// 私服列表：维护者
+
+const CrystalGamePanel: React.FC = () => {
+
+  const containerCss = {
+    width: '800px',
+    height: '400px',
+  }
+
+  return (
+    <div style={containerCss}>
+      <GamePanel appId="548430"></GamePanel>
+    </div>
+  )
+};
+
+export default CrystalGamePanel;
