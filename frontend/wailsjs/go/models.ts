baab33a3a9fdc94b7038eb2153689ebef8b5820c
bulletjet <851234786@qq.com>
2025-02-28 16:42:18 +0800

    add currency client; fix generate binding failed

---
+++ frontend/wailsjs/go/models.ts
@@ -0,0 +1,38 @@
+export namespace common {
+	
+	export class Status {
+	    state: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new Status(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.state = source["state"];
+	    }
+	}
+
+}
+
+export namespace machine {
+	
+	export class Info {
+	    id: string;
+	    os: string;
+	    arch: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new Info(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.os = source["os"];
+	        this.arch = source["arch"];
+	    }
+	}
+
+}
+
