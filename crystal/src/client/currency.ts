d050b587a63124091477ca8b22ddd8f0c7094db9
bulletjet <851234786@qq.com>
2025-03-10 10:07:16 +0800

    add cache for rate

--- crystal/src/client/currency.ts
+++ crystal/src/client/currency.ts
@@ -1,13 +1,18 @@
 export class CurrencyClient {
 
     // curl 'https://p.deckz.fun/currency/exchange_rate/convert?fromCode=USD&toCode=CNY&money=1'
-    apiBase: string = "https://p.deckz.fun"; 
+    apiBase: string = "https://p.deckz.fun";
+
+    private cache = new Map<string, number>();
 
     constructor() {}
 
     async rate(fromCode: string, toCode: string): Promise<number | null> {
         if (fromCode == toCode) {
             return 1;
+        }
+        if (this.cache.has(this.cacheKey(fromCode, toCode))) {
+            return this.cache.get(this.cacheKey(fromCode, toCode));
         }
         const response = await fetch(`${this.apiBase}/currency/exchange_rate/convert?fromCode=${fromCode}&toCode=${toCode}&money=1`)
         const rateData: CurrencyResponse = await response.json()
@@ -16,7 +21,12 @@ export class CurrencyClient {
         if (!rateData.success) {
             return null;
         }
+        this.cache.set(this.cacheKey(fromCode, toCode), rateData.data.money);
         return rateData.data.money;
+    }
+
+    cacheKey(fromCode: string, toCode: string) {
+        return `${fromCode}-${toCode}`
     }
 }
 
