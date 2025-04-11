5c17e033fb293154a49424496979283cf6dba79b
bulletjet <851234786@qq.com>
2025-04-11 16:53:52 +0800

    udpate

--- main.go
+++ main.go
@@ -31,11 +31,6 @@ var trayIcon []byte
 const defaultRemoteDebuggingUrl = "http://localhost:8080"
 
 func main() {
-
-	//var updater = &selfupdater.Updater{
-	//	CurrentVersion: version,
-	//}
-
 	mode := flag.String("mode", constants.UserRunMode, "启动方式")
 	flag.Parse()
 	windowStartState := options.Normal
